2d4d3e56e4989b5430479dea0b685a3c173bb5ba
berwyn <berwyn@f207b584-60fb-0310-94b1-82f73087e710>
2015-05-27 21:48:47 +0000

    Rename folder called "Songs/Psalms and Hymns PDF" to "Songs/PsalmsPDF" since that is all it contains now.  The hymn PDFs are elsewhere.

--- noprint.bat
+++
@@ -1,1 +0,0 @@
-pdftk "psalm006_LORD,_chasten_not_in_anger.pdf" output "psalm006_LORD,_chasten_not_in_anger.np.pdf" owner_pw "hymnal" allow ModifyContents allow CopyContents allow ScreenReaders allow ModifyAnnotations
