58b490ce340fdbfc017c50c6006574d367de0fda
berwyn <berwynhoyt@users.noreply.github.com>
2015-05-27 21:34:37 +0000

    Add the beginnings of a script to turn off print for FACR pdf files.

---
+++ noprint.bat
@@ -0,0 +1,1 @@
+pdftk "psalm006_LORD,_chasten_not_in_anger.pdf" output "psalm006_LORD,_chasten_not_in_anger.np.pdf" owner_pw "hymnal" allow ModifyContents allow CopyContents allow ScreenReaders allow ModifyAnnotations
