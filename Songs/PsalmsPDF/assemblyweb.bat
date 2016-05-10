5e55468c49c4e0d33d3da64a8bb5c903af293a8c
berwyn <berwynhoyt@users.noreply.github.com>
2016-05-10 23:10:10 +0000

    Make web assembly batch file work with new Java, too.

--- Songs/PsalmsPDF/assemblyweb.bat
+++ Songs/PsalmsPDF/assemblyweb.bat
@@ -5,16 +5,14 @@ rem Exclude Ps. 16 specifically because it has 16b on it
 echo Renaming "psalm016_Protect_me,_O_my_God.pdf"
 ren "psalm016_Protect_me,_O_my_God.pdf" "psalm016_Protect_me,_O_my_God.pdf.temp"
 
-call java tool.pdf.Merge Title-web.pdf Copyright.pdf Preface.pdf blankpage.pdf psalm*.pdf Based_on_Psalms.pdf Hymn*.pdf Copyright_Holders.pdf Index.pdf
-call java tool.pdf.Compress Title-web-m.pdf
+call assembly.bat Title-web
 
 echo Restoring "psalm016_Protect_me,_O_my_God.pdf"
 ren "psalm016_Protect_me,_O_my_God.pdf.temp" "psalm016_Protect_me,_O_my_God.pdf"
 for %%f in (..\*.withheld) do call :rename2 "%%~nf"
 
 echo on
-del Title-web-m.pdf
-ren Title-web-m-o.pdf "Sing to the Lord - provisional - web.pdf"
+move /y Title-web-m-o.pdf "Sing to the Lord - provisional - web.pdf"
 goto :EOF
 
 :rename1
