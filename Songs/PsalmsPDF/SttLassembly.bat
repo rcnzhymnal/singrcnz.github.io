d6fe1ebb1df27ae307b32b90ed1fe8bafbe8816d
nugteren <nugteren@f207b584-60fb-0310-94b1-82f73087e710>
2016-05-14 09:58:06 +0000

    Adding current versions of various pdf's that are used in SttL but aren't on the wiki.

---
+++ Songs/PsalmsPDF/SttLassembly.bat
@@ -0,0 +1,4 @@
+java tool.pdf.Merge SttLfrontpages.pdf SubtitlePsalms.pdf blankpage.pdf psalm*.pdf blankpage.pdf SubtitleHymn.pdf Hymn*.pdf SttLbackpages1.pdf Guitar.pdf SttLbackpages2.pdf
+java tool.pdf.Compress SttLfrontpages-m.pdf
+pause
+
