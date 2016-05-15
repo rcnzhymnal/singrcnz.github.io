3b721f616802bc22db939c6135bc5053eedf7b32
nugteren <lydia.nugteren@gmail.com>
2016-05-15 10:11:33 +0000

    This works to make the big PDF but doesn't revert the hymn np files.  Sorry, you'll have to fix it, Ber.

--- Songs/PsalmsPDF/SttLassembly.bat
+++ Songs/PsalmsPDF/SttLassembly.bat
@@ -1,8 +1,21 @@
 rem Build SttL hymnal from pdfs
 
+rem Temporarily get rid of 'non-publishable' (.np) files during assembly process
+ren *.np.pdf *.tmp
+ren ..\..\Hymns\*.np.pdf *.tmp
+
+pause
+
+
 call SttL-psalm-list
 call java -cp * tool.pdf.Merge SttLfrontpages.pdf SubtitlePsalms.pdf blankpage.pdf %psalms% blankpage.pdf SubtitleHymn.pdf ..\..\Hymns\Hymn*.pdf SttLbackpages1.pdf Guitar.pdf SttLbackpages2.pdf
 call java -cp * tool.pdf.Compress SttLfrontpages-m.pdf
 del SttLfrontpages-m.pdf
 move /y SttLfrontpages-m-o.pdf "SttL.pdf" && echo Created "SttL.pdf"
+
+rem Restore 'non-publishable' (.np) files
+ren *.np.tmp *.pdf
+
+
+
 pause
