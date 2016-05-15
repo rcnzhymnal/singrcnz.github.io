87de2c3ddb4e41f916acbca6e8f8bcc845672ab2
berwyn <berwyn@f207b584-60fb-0310-94b1-82f73087e710>
2016-05-15 04:32:56 +0000

    Adjusted SttLassembly.bat to produce the full hymnal

--- Songs/PsalmsPDF/assemblyweb.bat
+++ Songs/PsalmsPDF/assemblyweb.bat
@@ -5,14 +5,22 @@ rem Exclude Ps. 16 specifically because it has 16b on it
 echo Renaming "psalm016_Protect_me,_O_my_God.pdf"
 ren "psalm016_Protect_me,_O_my_God.pdf" "psalm016_Protect_me,_O_my_God.pdf.temp"
 
-call assembly.bat Title-web
+rem Temporarily get rid of 'non-publishable' (.np) files during assembly process
+ren *.np.pdf *.tmp
+
+call SttL-psalm-list
+call java -cp * tool.pdf.Merge Title-web.pdf Copyright.pdf Preface.pdf blankpage.pdf %psalms% Based_on_Psalms.pdf Copyright_Holders.pdf Index.pdf
+call java -cp * tool.pdf.Compress Title-web-m.pdf
+
+rem Restore 'non-publishable' (.np) files
+ren *.np.tmp *.pdf
 
 echo Restoring "psalm016_Protect_me,_O_my_God.pdf"
 ren "psalm016_Protect_me,_O_my_God.pdf.temp" "psalm016_Protect_me,_O_my_God.pdf"
 for %%f in (..\*.withheld) do call :rename2 "%%~nf"
 
-echo on
-move /y Title-web-m-o.pdf "Sing to the Lord - provisional - web.pdf"
+del Title-web-m.pdf
+move /y Title-web-m-o.pdf "Sing to the Lord - provisional - web.pdf" && echo Created "Sing to the Lord - provisional - web.pdf"
 goto :EOF
 
 :rename1
