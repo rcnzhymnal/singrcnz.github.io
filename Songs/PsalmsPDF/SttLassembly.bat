4c48ef92564e4c0ef9655a43759c24885211df50
berwyn <berwynhoyt@users.noreply.github.com>
2016-05-15 04:32:56 +0000

    Adjusted SttLassembly.bat to produce the full hymnal

--- Songs/PsalmsPDF/SttLassembly.bat
+++ Songs/PsalmsPDF/SttLassembly.bat
@@ -1,4 +1,8 @@
-java tool.pdf.Merge SttLfrontpages.pdf SubtitlePsalms.pdf blankpage.pdf psalm*.pdf blankpage.pdf SubtitleHymn.pdf Hymn*.pdf SttLbackpages1.pdf Guitar.pdf SttLbackpages2.pdf
-java tool.pdf.Compress SttLfrontpages-m.pdf
-pause
+rem Build SttL hymnal from pdfs
 
+call SttL-psalm-list
+call java -cp * tool.pdf.Merge SttLfrontpages.pdf SubtitlePsalms.pdf blankpage.pdf %psalms% blankpage.pdf SubtitleHymn.pdf ..\..\Hymns\Hymn*.pdf SttLbackpages1.pdf Guitar.pdf SttLbackpages2.pdf
+call java -cp * tool.pdf.Compress SttLfrontpages-m.pdf
+del SttLfrontpages-m.pdf
+move /y SttLfrontpages-m-o.pdf "SttL.pdf" && echo Created "SttL.pdf"
+pause
